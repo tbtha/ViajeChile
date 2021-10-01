0b4dfb7a8e2ba091cf3180ed4751d9776f6bc068
Tabatha Gamboa <tabathagamboa9@gmail.com>
2021-09-30 22:55:14 -0300

    Integracion javascript

--- aseets/js/script.js
+++ aseets/js/script.js
@@ -1,2 +1,17 @@
 $(document).ready(function(){
+    console.log("ready!");
+    //integrar color al navbar en movimiento  
+    $(window).scroll(function () {
+        if ($("#navbar").offset().top > 56) {
+            $("#navbar").addClass("navbar-color");
+        } else {
+            $("#navbar").removeClass("navbar-color");
+        }
+    });
+    
+
+    $("#img").click(function(){
+        $(".card-body").toggle();
+
+});
 });
